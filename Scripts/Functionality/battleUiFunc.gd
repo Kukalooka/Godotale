82f3b248c2699a66cb9eb6bb0eae5abd46cf9526
Aleksander Drąg <kukalooka@icloud.com>
2025-05-15 21:39:41 +0200

    Added SFX to the buttons

--- Scripts/Functionality/battleUiFunc.gd
+++ Scripts/Functionality/battleUiFunc.gd
@@ -2,8 +2,22 @@ extends Control
 
 class_name BattleUi
 
+@export_file("*.wav") var uiMoveSfx : String
+@export_file("*.wav") var uiConfirmSfx : String
+
 func _ready():
 	$FightButton.grab_focus()
+	for x in get_children():
+		if "button" in x.name.to_lower():
+			if x.has_signal("pressed"):
+				x.focus_entered.connect(func ():
+					$AudioStreamPlayer2D.stream = load(uiMoveSfx)
+					$AudioStreamPlayer2D.play()
+				)
+				x.pressed.connect(func ():
+					$AudioStreamPlayer2D.stream = load(uiConfirmSfx)
+					$AudioStreamPlayer2D.play()
+				)
 
 func changeHpBar(curHp):
 	$HP_Bar.value = curHp
