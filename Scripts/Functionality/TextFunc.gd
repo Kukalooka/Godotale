26102b95eae90d5f21b3e8aa4c88b836a6f73d03
Aleksander Drąg <kukalooka@icloud.com>
2025-06-07 17:50:57 +0200

    Added support for BBCodes

--- Scripts/Functionality/TextFunc.gd
+++ Scripts/Functionality/TextFunc.gd
@@ -6,12 +6,28 @@ class_name TextboxSystem
 
 func _writeToBox(text : String, interval, sound):
 	$AudioStreamPlayer2D.stream = load(sound)
+	var bracketflag = 0
+	var bbcode = ""
 	for x in range(len(text)):
-		$RichTextLabel.text += text[x]
-		if text[x] != " ":
-			$AudioStreamPlayer2D.play()
-		await get_tree().create_timer(interval).timeout
-		
+		if text[x] != "[" and bracketflag == 0:
+			$RichTextLabel.append_text(text[x])
+			if text[x] != " ":
+				$AudioStreamPlayer2D.play()
+			await get_tree().create_timer(interval).timeout
+		else:
+			if bracketflag < 1:
+				if text[x+1] == "/":
+					bracketflag = 2
+				else:
+					bracketflag = 1
+			bbcode += text[x]
+			
+			if text[x] == "]":
+				print("Textbox: ", $RichTextLabel.text, " BBCode: ", bbcode)
+				$RichTextLabel.append_text(bbcode)
+				bbcode = ""
+
+				bracketflag = 0
 		
 
 	
